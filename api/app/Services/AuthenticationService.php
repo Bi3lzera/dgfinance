c7c44cc69d301de2f7fb20d465e0c352974aa0a0
bi3l <videompx@gmail.com>
2025-01-28 19:50:26 -0300

    Some changes are made.

---
+++ api/app/Services/AuthenticationService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+
+class AuthenticationService
+{
+    public function doLogin(string $login, string $password): Response
+    {
+        $credentials = $this->refineLoginCredentials($login, $password);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token')->plainTextToken;
+
+            $user->type = $user->getType();
+            $user = $user->toArray();
+
+            return new Response(['user' => $user, 'token' => $token], Response::HTTP_OK);
+        }
+
+        return new Response(['message' => 'Credenciais inválidas.'], Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function getAuthenticatedUser()
+    {
+        $user = Auth::user();
+
+        $user->type = $user->getType();
+        $user = $user->toArray();
+
+        return new Response($user, Response::HTTP_OK);
+    }
+
+    public function logout(): Response
+    {
+        Auth::user()->currentAccessToken()->delete();
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    private function refineLoginCredentials(string $login, string $password): array
+    {
+        $loginType = filter_var($login, FILTER_VALIDATE_EMAIL) ? 'email' : 'cpf';
+
+        return [
+            $loginType => $login,
+            'password' => $password
+        ];
+    }
+}
---
+++ api/routes/api.php
@@ -0,0 +1,6 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('login', [AuthenticationController::class, 'login']);
